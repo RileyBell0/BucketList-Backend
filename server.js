77c90cacaff7f3eb19cab239faf013a2b4daf2bd
Riley Bell <56055271+RileyBell0@users.noreply.github.com>
2022-11-21 15:27:12 +1100

    Vercel test

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const port = process.env.PORT || 8080;
 const app = require("./app");
 
 app.listen(port, () => {
-  console.log("Listening on port " + port);
+  console.log("listening on port " + port);
 });
