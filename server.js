04a45c5da3f606fa2290f64a41e13c3694e61ea8
Riley Bell <56055271+RileyBell0@users.noreply.github.com>
2022-11-21 17:39:41 +1100

    Express CORS Test

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const port = process.env.PORT || 8080;
 const app = require("./index");
 
 app.listen(port, () => {
-  console.log("listening on port " + port);
+  console.log("Listening on port " + port);
 });
